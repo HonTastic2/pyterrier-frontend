989daf51c2ad0af00eb52e4480d84cacc6651988
Hugh O'Neill <2579978o@student.gla.ac.uk>
2024-10-31 16:05:24 +0000

    Add text input and button that displays it on click

--- pyterrier-frontend/src/App.js
+++ pyterrier-frontend/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [inputText, setInputText] = useState('');
+  const [showResult, setShowResult] = useState(false);
+
+  const handleSearch = () => {
+    setShowResult(true);
+  };
+
+  const goBack = () => {
+    setShowResult(false);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Background Link Finder</p>
       </header>
+      <body className="App-body">
+        <div>
+          { showResult ? (<>
+          <p>{inputText}</p>
+          <button className="Button" onClick={goBack}>Back</button>
+          </>) : (<>
+          <p>Enter article contents:</p>
+          <textarea name="inputArticle" value={inputText} onChange={(e) => setInputText(e.target.value)} rows={20} cols={100}/>
+          <br />
+          <button className="Button" onClick={handleSearch}>Search</button>
+          </>)}
+        </div>
+      </body>
     </div>
   );
 }
